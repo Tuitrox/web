5f98e200d50bfa139e160fdf6726df17ace5a3b2
Tuitrox <tuitrox@gmail.com>
2025-12-26 06:31:46 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+onst btn = document.getElementById('btn');
+const title = document.getElementById('title');
+btn.addEventListener('click', () => {
+    title.textContent = 'Текст изменен через JS!';
+    title.style.color = 'red';
+});
